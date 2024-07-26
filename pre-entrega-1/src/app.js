ea9aad41012033dc3bbd9abc079827fee495c264
Pablo Bellandi <pablohbell1@gmail.com>
2024-07-25 23:32:24 -0300

    update

--- pre-entrega-1/src/app.js
+++ pre-entrega-1/src/app.js
@@ -7,13 +7,13 @@ const PORT = 8080;
 const app = express();
 
 import productsRouter from "./routes/products.router.js";
-import cartsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js";
 
 
 app.use(express.json());
 
-app.use("/", productsRouter);
-app.use("/", cartsRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 
 
 
@@ -21,26 +21,3 @@ app.use("/", cartsRouter);
 app.listen(PORT, () => {
     console.log(`escuchando en el puerto ${PORT}`);
 })
-
-
-
-/* visualizar por ID GET
-app.get("/products/:id", (req, resp) => {
-    let id = req.params.id;
-    console.log(id);
-    let searchProd = misProductos.find(product => product.id == id);
-    if (searchProd) {
-        res.send(searchProd);
-    } else {
-        resp.send("Item No Encontrado")
-    }
-})
-*/
-
-// METODO POST CARGAMOS NUEVO PRODUCTO AL ARRAY
-/*app.post("/", (req, res) => {
-    const newProduct = req.body;
-    products.push(newProduct);
-
-    res.send({status: "success", message:"Producto Agregado"});
-})*/
